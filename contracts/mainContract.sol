98cefbb220acb81d5c7b4d79919e770efe2177d3
Hyunjae Lee <hl285@duke.edu>
2018-07-05 15:03:17 +0200

    added balances mapping

--- contracts/mainContract.sol
+++ contracts/mainContract.sol
@@ -212,7 +212,7 @@ contract TradeRegulation is Ownable{
    }
    mapping(bytes32=>Tx) trades;
    mapping(bytes32=>uint) amountCondition;
-
+   mapping(address=>uint) balances;
 
 
    event TradeCreated(bytes32 id);
@@ -277,14 +277,19 @@ contract TradeRegulation is Ownable{
    }
      if(now>trades[id].shippingDate+trades[id].loc.numDays && trades[id].shippingDate<(trades[id].locIssueDate+trades[id].loc.numDays) && isRightParties(id)) {
        trades[id].ethAddressByRole["seller"].transfer(trades[id].loc.creditAmt);
+       balances[trades[id].ethAddressByRole["seller"]]+=trades[id].loc.creditAmt;
+       balances[trades[id].ethAddressByRole["buyer"]]-=trades[id].loc.creditAmt;
        SellerPayed(id, true);
      }
+
      SellerPayed(id, false);
    }
 
 
    function payToInsurer(bytes32 id) {
      trades[id].ethAddressByRole["insurer"].transfer(trades[id].insuranceAmt);
+     balances[trades[id].ethAddressByRole["insurer"]]+=trades[id].insuranceAmt;
+     balances[trades[id].ethAddressByRole["buyer"]]-=trades[id].insuranceAmt;
      InsurerPayed(id);
    }
 
@@ -299,7 +304,7 @@ contract TradeRegulation is Ownable{
 
 
    function depositFunds(bytes32 id) payable{
-
+    balances[trades[id].ethAddressByRole["buyer"]]+=msg.value;
     DepositMade(id, msg.value);
    }
 }
